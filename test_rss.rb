17ad50f53748a1e133dbf04b1f5932771ca1c33a
Diego Diez <diego10ruiz@gmail.com>
2010-01-29 10:10:17 +0000

    git-svn-id: file:///Volumes/Biodev/svn/seqminer@364 c425adfb-4092-4a75-b4df-32cab5f874b6

--- test_rss.rb
+++ test_rss.rb
@@ -3,7 +3,8 @@ require 'open-uri'
 
 #feed  = RSS::Parser.parse( 'http://code.google.com/feeds/p/vardbdev/issueupdates/basic')
 #feed  = RSS::Parser.parse('http://groups.google.com/group/vardb-devel/feed/atom_v1_0_msgs.xml')
-feed  = RSS::Parser.parse('http://groups.google.com/group/vardb-devel/feed/rss_v2_0_msgs.xml')
+#feed  = RSS::Parser.parse('http://groups.google.com/group/vardb-devel/feed/rss_v2_0_msgs.xml')
+feed  = RSS::Parser.parse('http://groups.google.com/group/vardb-devel/feed')
 puts "<h1>" + feed.title.content + "</h1>"
 n = 0
 feed.items.each do |item|
