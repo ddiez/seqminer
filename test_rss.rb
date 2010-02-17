67a243e1f4d97d6e2e958751bcfc3f6f4afba475
Diego Diez <diego10ruiz@gmail.com>
2010-02-17 06:38:43 +0000

    git-svn-id: file:///Volumes/Biodev/svn/seqminer@400 c425adfb-4092-4a75-b4df-32cab5f874b6

--- test_rss.rb
+++ test_rss.rb
@@ -1,17 +1,38 @@
 require 'rss'
 require 'open-uri'
 
-#feed  = RSS::Parser.parse( 'http://code.google.com/feeds/p/vardbdev/issueupdates/basic')
-#feed  = RSS::Parser.parse('http://groups.google.com/group/vardb-devel/feed/atom_v1_0_msgs.xml')
-#feed  = RSS::Parser.parse('http://groups.google.com/group/vardb-devel/feed/rss_v2_0_msgs.xml')
-feed  = RSS::Parser.parse('http://groups.google.com/group/vardb-devel/feed')
-puts "<h1>" + feed.title.content + "</h1>"
+# This is Atom.
+#feed = RSS::Parser.parse( 'http://code.google.com/feeds/p/vardbdev/issueupdates/basic')
+
+# Requires the Feed to be public! (unless I can find a way around).
+#feed = RSS::Parser.parse('http://groups.google.com/group/vardb-devel/feed/atom_v1_0_msgs.xml')
+#feed = RSS::Parser.parse('http://groups.google.com/group/vardb-devel/feed/rss_v2_0_msgs.xml')
+
+feed = RSS::Parser.parse('http://groups.google.com/group/vardb-test/feed/rss_v2_0_msgs.xml')
+#feed = RSS::Parser.parse('http://groups.google.com/group/vardb-test/feed/atom_v1_0_msgs.xml')
+
+# For RSS.
+puts "<h1>" + feed.channel.title + "</h1>"
 n = 0
 feed.items.each do |item|
 	break if n == 5
-	puts "<h3>" + item.title.content + "</h3>"
-	puts item.content.content
-	puts "<a href=" + item.link.href + ">View more...</a>"
+	puts "<h3>" + item.title + "</h3>"
+	puts item.description
+	puts "<a href=" + item.link + ">View more...</a>"
 	puts
 	n += 1
 end
+
+# For Atom.
+# This is not working for google groups: item.content is nil!.
+# It does work for google code.
+#puts "<h1>" + feed.title.content + "</h1>"
+#n = 0
+#feed.items.each do |item|
+#	break if n == 5
+#	puts "<h3>" + item.title.content + "</h3>"
+#	puts item.content.content
+#	puts "<a href=" + item.link.href + ">View more...</a>"
+#	puts
+#	n += 1
+#end
