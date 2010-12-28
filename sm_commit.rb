ff85dc613e6e751d5a9696a9cc975130c982869e
Diego Diez <diego10ruiz@gmail.com>
2010-12-28 05:23:04 +0000

    - added new release script sm_commit.rb
    git-svn-id: file:///Volumes/Biodev/svn/seqminer@469 c425adfb-4092-4a75-b4df-32cab5f874b6

---
+++ sm_commit.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#
+# SeqMiner is a tool for mining sequence information. It aims to
+# help detect sequences belonging to specific protein families.
+#
+# Author::    Diego Diez  (mailto:diez@kuicr.kyoto-u.ac.jp)
+# Copyright:: Copyright (c) 2010
+# License::   Distributes under the same terms as Ruby
+
+require 'seqminer'
+# 1. create object.
+c = SeqMiner::Commit.new
+c.debug
+# 2. commit files to directory
+c.commit
+# 3. get stats.
+c.stat_sequences
