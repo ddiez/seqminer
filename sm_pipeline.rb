1d4f407e67ef1f69a0a446e87004cb543effa53b
Diego Diez <diego10ruiz@gmail.com>
2010-12-28 05:20:48 +0000

    git-svn-id: file:///Volumes/Biodev/svn/seqminer@468 c425adfb-4092-4a75-b4df-32cab5f874b6

--- sm_pipeline.rb
+++ sm_pipeline.rb
@@ -9,29 +9,41 @@
 
 require 'seqminer'
 
-sm = SeqMiner::Pipeline.new
+# 1.1 create pipelie object.
+sm = SeqMiner::Pipeline.new("vardb-dr-6")
 
-# filter taxons.
-sm.taxon.filter_by_name("plasmodium.falciparum")
+# 1.2 optional: filter taxons.
+#sm.taxon.filter_by_name("anaplasma.marginale")
 #sm.taxon.filter_by_type("spp")
-sm.taxon.filter_by_type("clade")
+#sm.taxon.debug
 
-# If first time and not run_all()...
-#sm.dir_initialize
+# 1.3 optional: filter families.
+#sm.ortholog.filter_by_name("msp2_p44_map1_omp")
+#sm.ortholog.debug
 
-# filter orthologs.
-sm.ortholog.filter_by_name("var")
-
-# search.
-#sm.build_search
-#sm.search.debug
+## run all.
+# 2.1 run search, get results, write reports, run scan, get results, write reports.
 #sm.run_all
-#sm.run_search
-#sm.write_fasta
-#sm.write_nelson
 
-# scan.
-sm.build_scan
-sm.scan.debug
-sm.run_scan
-sm.write_domain
+## run step by step (instead of run_all above)
+# 2.2 optional: search
+#sm.build_search
+#sm.run_search
+#sm.get_search_results
+#sm.write_nelson
+#sm.write_fasta
+
+# 2.3 optional: scan (requires search above)
+#sm.build_scan
+#sm.run_scan
+#sm.get_scan_results
+#sm.write_domain
+
+# 3.1 report
+# TODO: check all the reports.
+# TODO: report to vardb dir.
+sm.build_search
+sm.get_search_results(eval = 0.01)
+#sm.report
+#sm.report(0.8)
+sm.report_summary
